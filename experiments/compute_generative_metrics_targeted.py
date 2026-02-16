f0766171ee7c1417c6a10257562a7eec1b2c938a
Konstantinos Kevopoulos <kevopoulos.kon@gmail.com>
2026-02-16 18:09:35 +0100

    minor changes

---
+++ experiments/compute_generative_metrics_targeted.py
@@ -0,0 +1,88 @@
+from utils.measure_mesh_qualities import MMD, Coverage
+import numpy as np
+import pyvista as pv
+from tqdm import tqdm
+import random
+
+
+random.seed(42)
+np.random.seed(42)
+
+
+
+
+def load_pcs_gen(path, num_momenta_samples):
+
+    points_all = []
+
+    pbar = tqdm(total=num_momenta_samples, desc='Loading pcs...')
+    for i in range(num_momenta_samples):
+        filename = path + f"/PointCloud_{i}.vtk"
+
+        points = pv.read(filename).points
+        points = np.array(points)
+
+        points_all.append(points)
+
+        pbar.update()
+    pbar.close()
+
+    return  points_all
+
+
+
+
+
+def load_pcs_real(num_momenta_samples):
+
+    points_all = []
+
+    pbar = tqdm(total=num_momenta_samples, desc='Loading pcs...')
+    for i in range(num_momenta_samples):
+        path = "/home/kostas/home/Gen_Metrics_Exp_Files_Targeted/Real_PCs"
+        filename = path + f"/PointCloud_{i}.vtk"
+
+        points = pv.read(filename).points
+        points = np.array(points)
+
+        points_all.append(points)
+
+        pbar.update()
+    pbar.close()
+
+    return  points_all
+
+
+path_init = "/home/kostas/home/Gen_Metrics_Exp_Files_Targeted"
+
+models = ['nf', 'vae2', 'vae3']
+
+real_pcs = load_pcs_real(num_momenta_samples=699)
+N_subsample = 650 ### subsample the real dataset, so that the real and generated sets have equal size
+
+num_runs_stoch = 4
+mmd_vals_all = []
+cov_vals_all = []
+for i in range(num_runs_stoch):
+    real_pcs_subsampled = random.sample(real_pcs, N_subsample)
+
+    mmd_vals = []
+    cov_vals = []
+    for model in models:
+
+        ### gen_pcs now will have 650 samples
+        gen_pcs = load_pcs_gen(path=path_init+f"/{model}_PCs", num_momenta_samples=650)
+
+        mmd = MMD(S_g=gen_pcs, S_r=real_pcs_subsampled)
+        cov = Coverage(S_g=gen_pcs, S_r=real_pcs_subsampled)
+
+        mmd_vals.append(mmd)
+        cov_vals.append(cov)
+
+        print(model, mmd, cov)
+
+    mmd_vals_all.append(mmd_vals)
+    cov_vals_all.append(cov_vals)
+
+np.save("data_models_saved/data/mmd_vals_targeted.npy", np.array(mmd_vals_all))
+np.save("data_models_saved/data/cov_vals_targeted.npy", np.array(cov_vals_all))
--- utils/rearrange_files_targeted_genmetrics.py
+++ utils/rearrange_files_targeted_genmetrics.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 from tqdm import tqdm
+import pandas as pd
+import numpy as np
 
 
 
@@ -33,6 +35,52 @@ def copy2clean(model_str, all_num, model_str_path):
 
 
 os.makedirs( r"C:\Users\kkevopoulos\Documents\Meshes_Anatomies\Gen_Metrics_Exp_Files_Targeted", exist_ok=True)
+
+### real patients of the subgroup
+x_confounders = pd.read_excel(r"C:\Users\kkevopoulos\OneDrive - Delft University of Technology\Bureaublad\data_kostas_bivme\metadata_final.xlsx")
+x_confounders.drop(['Participant ID', 'Height', 'Weight', 'Diastolic BP',
+                    'Systolic BP', 'Unnamed: 8', 'Unnamed: 9', 'subject_id'], axis=1, inplace=True)
+x_confounders = pd.get_dummies(x_confounders, columns=['Sex'])
+x_confounders['Sex_Female'] = x_confounders['Sex_Female'].replace({True: 1, False: 0})
+x_confounders['Sex_Male'] = x_confounders['Sex_Male'].replace({True: 1, False: 0})
+x_confounders = x_confounders[['BMI', 'Age', 'Sex_Female', 'Sex_Male']]
+x_confounders = x_confounders.to_numpy()
+
+### Find the indices of the real subgroup
+male_indices = np.where(x_confounders[:, 2] == 0)[0]
+x_conf_male = x_confounders[male_indices]
+
+subgroup_indices = np.where(x_conf_male[:, 1] > 58)[0]
+
+os.makedirs(fr"C:\Users\kkevopoulos\Documents\Meshes_Anatomies\Gen_Metrics_Exp_Files_Targeted\Real_PCs", exist_ok=True)
+
+NumAll = 2274
+j=0
+pbar = tqdm(total=NumAll, desc='Copy PCs to clean folder...')
+for i in range(NumAll):
+    if i in subgroup_indices:
+
+        vtk_src_path = fr"C:\Users\kkevopoulos\Documents\Meshes_Anatomies\Reference_Momenta\Shooting_Momenta_{i}\output\Shooting__GeodesicFlow__biv__tp_10__age_1.00.vtk"
+        new_folder_path = fr"C:\Users\kkevopoulos\Documents\Meshes_Anatomies\Gen_Metrics_Exp_Files_Targeted\Real_PCs\PointCloud_{j}.vtk"
+
+        # Ensure source exists
+        if not os.path.exists(vtk_src_path):
+            print("WARNING: source missing:", vtk_src_path)
+            pbar.update()
+            continue
+
+        try:
+            shutil.copy(src=vtk_src_path, dst=new_folder_path)
+        except PermissionError as e:
+            print("Permission error copying to", new_folder_path, "-", e)
+        except Exception as e:
+            print("Copy failed:", e)
+        j += 1
+
+    pbar.update()
+pbar.close()
+
+
 
 num_gen = 650 ### 650 synthetic anatomies for each model
 
